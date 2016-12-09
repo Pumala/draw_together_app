82caf040853db521bd65823c05d7ec6bc1f6839e
Carolyn Lam <caro_lyn_@hotmail.com>
2016-12-09 18:41:16 -0500

    add comments to backend.js

--- backend.js
+++ backend.js
@@ -6,12 +6,14 @@ const express     = require('express'),
 app.use(express.static('public'));
 
 
+// once a user is connected
 io.on('connection', function(socket) {
+  // emit the welcome event, passing in a greeting that you want to send out to all users who are connected
   socket.emit('welcome', 'Welcome to the Drawing Together App!');
 
+  // captures the drawing event and receives the coords
   socket.on('drawing', function(coords) {
-    console.log('how do you draw?');
-    console.log('coords:', coords);
+    // once recieved, sends a the coords to all users except self
     socket.broadcast.emit('broadcast', coords);
   })
 
